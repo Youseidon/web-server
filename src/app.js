a33d4d18646a36cbb68d490ee8fb7257f4960364
Yousef Nourizadeh <nou.yousef@gmail.com>
2020-05-21 16:14:41 +0400

    web server styled and partialized

--- src/app.js
+++ src/app.js
@@ -1,15 +1,18 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express();
 
 // Define paths for express config
 const dirStaticPath = path.join(__dirname, '../public')
-const viewDirectory = path.join(__dirname, '../templates')
+const viewDirectory = path.join(__dirname, '../templates/Views')
+const partialPath = path.join(__dirname, '../templates/partials')
 
 // Setup handlebars engine and views location
 app.set('view engine', 'hbs');
 app.set('views', viewDirectory)
+hbs.registerPartials(partialPath) // contains the path that handlebars module needs
 
 // Setup static directory to serve
 app.use(express.static(dirStaticPath));
@@ -17,25 +20,43 @@ app.use(express.static(dirStaticPath));
 app.get('', (req, res) => {
     res.render('index', {
         title: 'Weather app',
-        name: 'Yousef'
+        name: 'Yousef Nourizadeh'
     });
 })
 app.get('/about', (req, res) => {
     res.render('about', {
         title: 'About',
-        image: 'img/robot.png'
+        image: 'img/robot.png',
+        name: 'Yousef Nourizadeh'
     })
 })
 app.get('/help', (req, res) => {
     res.render('help', {
         message: 'This is a message for you to help. help. help. help',
-        title: 'Help'
+        title: 'Help',
+        name: 'Yousef Nourizadeh'
     })
 })
 app.get('/weather', (req, res) => {
     res.send({
         Naji: { name: 'salam', title: 'talam' },
         Nami: { name: 'khodafez', title: 'todafez' }
+    })
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404 Page',
+        errorMessage: 'Help article not found.',
+        name: 'Yousef Nourizadeh'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404 Page',
+        errorMessage: 'Page not found.',
+        name: 'Yousef Nourizadeh'
     })
 })
 
